dadb6714d9f4ea64abd65e0bfffd56f547c8f36a
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2020-10-15 21:33:42 +0200

    add CustomButton Add to Cart, make styling

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './collection-item.styles.scss';
 
 function CollectionItem({ id, name, price, imageUrl }) {
-	return (
-		<div className='collection-item'>
-			<div className='collection-item_image' style={{ backgroundImage: `url(${imageUrl})` }} />
-			<div className='collection-item_footer'>
-				<span className='collection-item_name'>{name}</span>
-				<span className='collection-item_price'>{price}</span>
-			</div>
-		</div>
-	);
+  return (
+    <div className='collection-item'>
+      <div className='collection-item_image' style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className='collection-item_footer'>
+        <span className='collection-item_name'>{name}</span>
+        <span className='collection-item_price'>{price}</span>
+      </div>
+      <CustomButton inverted>Add to cart</CustomButton>
+    </div>
+  );
 }
 
 export default CollectionItem;
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ isGoogleSignIn, children, ...restProps }) => (
-	<button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...restProps}>
-		{children}
-	</button>
+const CustomButton = ({ isGoogleSignIn, children, inverted, ...restProps }) => (
+  <button
+    className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+    {...restProps}
+  >
+    {children}
+  </button>
 );
 
 export default CustomButton;
