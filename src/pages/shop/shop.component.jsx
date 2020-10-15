c66d0eac69363b75fea723149603599425280e92
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2020-10-15 22:37:42 +0200

    updated shop_data id's, add cart utils

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-import { SHOP_DATA } from './shop.data';
+import SHOP_DATA from './shop.data';
 
 import './shop.styles.scss';
 
 class ShopPage extends React.Component {
-	constructor(props) {
-		super();
+  constructor(props) {
+    super();
 
-		this.state = {
-			collections: SHOP_DATA,
-		};
-	}
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
 
-	render() {
-		const { collections } = this.state;
-		return (
-			<main className='shop-page'>
-				{collections.map(({ id, ...otherCollectionProps }) => {
-					return <CollectionPreview key={id} {...otherCollectionProps} />;
-				})}
-			</main>
-		);
-	}
+  render() {
+    const { collections } = this.state;
+    return (
+      <main className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => {
+          return <CollectionPreview key={id} {...otherCollectionProps} />;
+        })}
+      </main>
+    );
+  }
 }
 
 export default ShopPage;
