616b939cbda2568a98d32ee609d6bd5f48c0949d
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2020-11-05 22:17:59 +0100

    add checkout page, handle go to checkout cart button

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import { createStructuredSelector } from 'reselect';
 import HomePage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/signin-singup/signin-singup.component.jsx';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
@@ -45,6 +49,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop/' component={ShopPage} />
+          <Route exact path='/checkout/' component={CheckoutPage} />
           <Route
             exact
             path='/signin'
