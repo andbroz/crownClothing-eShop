ae289777300c6fcc1dd62663952176f1eaf6306c
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2020-11-07 22:46:26 +0100

    normalize collection data, style collection component

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,20 +1,10 @@
 import memoize from 'lodash.memoize';
 import { createSelector } from 'reselect';
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jacktes: 3,
-  womens: 4,
-  mens: 5,
-};
-
 const selectShop = state => state.shop;
 
 export const selectShopCollections = createSelector([selectShop], shop => shop.collections);
 
-export const selectCollection = memoize((collectionUrlParam) =>
-  createSelector([selectShopCollections], collections =>
-    collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
-  );
+export const selectCollection = memoize(collectionUrlParam =>
+  createSelector([selectShopCollections], collections => collections[collectionUrlParam])
 );
