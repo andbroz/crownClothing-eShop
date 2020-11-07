2a92a01db822b02a379f7cf5747b8c07cc43c8b8
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2020-11-07 22:58:45 +0100

    refactor collections-overview selector for preview

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -4,11 +4,12 @@ import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-import { selectShopCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import './collections-overview.styles.scss';
 
 function CollectionsOverview({ collections }) {
+  console.log('collections', collections);
   return (
     <section className='collections-overview'>
       {collections.map(({ id, ...otherCollectionProps }) => {
@@ -19,7 +20,7 @@ function CollectionsOverview({ collections }) {
 }
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections,
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -5,6 +5,10 @@ const selectShop = state => state.shop;
 
 export const selectShopCollections = createSelector([selectShop], shop => shop.collections);
 
+export const selectCollectionsForPreview = createSelector([selectShopCollections], collections =>
+  Object.keys(collections).map(key => collections[key])
+);
+
 export const selectCollection = memoize(collectionUrlParam =>
   createSelector([selectShopCollections], collections => collections[collectionUrlParam])
 );
