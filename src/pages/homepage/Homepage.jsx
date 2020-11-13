475aaf427a6856c7da901ce9a7c4a000e52e8e7d
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2020-11-13 23:09:36 +0100

    add styled-components, modify HomePage styles using SC

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Directory from '../../components/directory/directory.component';
 
-import './homepage.scss';
+import { HomePageContainer } from './homepage.styles';
 
 const HomePage = () => {
-	return (
-		<div className='homepage'>
-			<Directory />
-		</div>
-	);
+  return (
+    <HomePageContainer>
+      <Directory />
+    </HomePageContainer>
+  );
 };
 
 export default HomePage;
