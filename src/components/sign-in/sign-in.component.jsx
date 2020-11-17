ccfff6716e2e22af11f656fe541263ea90c98a25
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2020-11-17 23:11:32 +0100

    convert sign-in to styled component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
 import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
-import './sign-in.styles.scss';
+import {
+  ButtonsBarContainer,
+  SignInContainer,
+  SignInTitle,
+} from './sign-in.styles';
 
 class SignIn extends React.Component {
   constructor() {
@@ -38,8 +43,8 @@ class SignIn extends React.Component {
     const { email, password } = this.state;
 
     return (
-      <section className='signin'>
-        <h2 className='title'>I already have an account</h2>
+      <SignInContainer>
+        <SignInTitle>I already have an account</SignInTitle>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
@@ -62,14 +67,14 @@ class SignIn extends React.Component {
             handleChange={this.handleChange}
             required
           />
-          <div className='buttons'>
+          <ButtonsBarContainer>
             <CustomButton type='submit'>Sign In</CustomButton>
-            <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
               Sign In with Google
             </CustomButton>
-          </div>
+          </ButtonsBarContainer>
         </form>
-      </section>
+      </SignInContainer>
     );
   }
 }
