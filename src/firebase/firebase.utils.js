c6f0d7adedf8d22623df7706be88bde256b6f49b
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2020-11-21 00:30:06 +0100

    move shop data to Firestore, implement data fetch from db and put into redux state

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,41 +3,70 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyBJhnFUy1HSAQegHWdF1_jsC0Jw1WHSjlw',
-	authDomain: 'crown-db-c1e7d.firebaseapp.com',
-	databaseURL: 'https://crown-db-c1e7d.firebaseio.com',
-	projectId: 'crown-db-c1e7d',
-	storageBucket: 'crown-db-c1e7d.appspot.com',
-	messagingSenderId: '443408388335',
-	appId: '1:443408388335:web:296ce8a0f799b21bf60b1a',
-	measurementId: 'G-E35N5WXSXE',
-};
-
-export const createUserProfileDocument = async (userAuth, additionalData) => {
-	if (!userAuth) return;
-
-	const userRef = firestore.doc(`users/${userAuth.uid}`);
-	const snapShot = await userRef.get();
-
-	if (!snapShot.exists) {
-		const { displayName, email } = userAuth;
-		const createdAt = new Date();
-
-		try {
-			await userRef.set({
-				displayName,
-				email,
-				createdAt,
-				...additionalData,
-			});
-		} catch (err) {
-			console.log('Error creating user: ', err.message);
-		}
-	}
-	return userRef;
+  apiKey: 'AIzaSyBJhnFUy1HSAQegHWdF1_jsC0Jw1WHSjlw',
+  authDomain: 'crown-db-c1e7d.firebaseapp.com',
+  databaseURL: 'https://crown-db-c1e7d.firebaseio.com',
+  projectId: 'crown-db-c1e7d',
+  storageBucket: 'crown-db-c1e7d.appspot.com',
+  messagingSenderId: '443408388335',
+  appId: '1:443408388335:web:296ce8a0f799b21bf60b1a',
+  measurementId: 'G-E35N5WXSXE',
 };
 
 firebase.initializeApp(firebaseConfig);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (err) {
+      console.log('Error creating user: ', err.message);
+    }
+  }
+  return userRef;
+};
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  return transformedCollection.reduce((acc, collection) => {
+    acc[collection.title.toLowerCase()] = collection;
+    return acc;
+  }, {});
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,13 +4,35 @@ import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
-function ShopPage({ match }) {
-  return (
-    <main className='shop-page'>
-      <Route exact path={`${match.path}`} component={CollectionsOverview} />
-      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-    </main>
-  );
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
+import { updateCollections } from '../../redux/shop/shop.actions';
+
+class ShopPage extends React.Component {
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const { updateCollections } = this.props;
+    const collectionRef = firestore.collection('collections');
+
+    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+    });
+  }
+
+  render() {
+    const { match } = this.props;
+    return (
+      <main className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+      </main>
+    );
+  }
 }
 
-export default ShopPage;
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap)),
+});
+export default connect(null, mapDispatchToProps)(ShopPage);
