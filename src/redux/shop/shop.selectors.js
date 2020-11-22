1cc1301378d11fce1293870854086f16e2785a05
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2020-11-22 18:29:21 +0100

    add WithSpinner HOC, use spinner for loading shop pages

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -6,9 +6,11 @@ const selectShop = state => state.shop;
 export const selectShopCollections = createSelector([selectShop], shop => shop.collections);
 
 export const selectCollectionsForPreview = createSelector([selectShopCollections], collections =>
-  Object.keys(collections).map(key => collections[key])
+  collections ? Object.keys(collections).map(key => collections[key]) : []
 );
 
 export const selectCollection = memoize(collectionUrlParam =>
-  createSelector([selectShopCollections], collections => collections[collectionUrlParam])
+  createSelector([selectShopCollections], collections =>
+    collections ? collections[collectionUrlParam] : null
+  )
 );
