96db7f9969e1bd0266cd50b6c86e71f8284a7d4a
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2020-12-05 22:51:00 +0100

    convert thunk to saga. refactor shop fetchcollectionstart action

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,7 +1,5 @@
 import ShopActionTypes from './shop.types';
 
-import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
-
 export const fetchCollectionsStart = () => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_START,
 });
@@ -15,19 +13,3 @@ export const fetchCollectionsFailure = errMsg => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
   payload: errMsg,
 });
-
-export const fetchCollectionsStartAsync = () => {
-  return dispatch => {
-    const collectionRef = firestore.collection('collections');
-
-    dispatch(fetchCollectionsStart());
-
-    collectionRef
-      .get()
-      .then(snapshot => {
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        dispatch(fetchCollectionsSuccess(collectionsMap));
-      })
-      .catch(err => dispatch(fetchCollectionsFailure(err.message)));
-  };
-};
